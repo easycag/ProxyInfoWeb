e9b8f8c60ee5016f399eaee09f19cd808d1e0956
Aashish <aashishgupta1@gmail.com>
2018-10-20 23:33:59 +0530

    Validation

---
+++ Models/GlobalValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProxyInfoWeb.Models
+{
+    public class GlobalValidation
+    {
+        public static bool ShouldNotNull(int iVal)
+        {
+            if (iVal <= 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
